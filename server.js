2cba2f9af634af9aafaa02a9f1135f4a5d2e712e
Sanskaar Patni <patnisanskaar@gmail.com>
2020-07-27 22:33:08 +0530

    Pushing to Heroku

--- server.js
+++ server.js
@@ -2,14 +2,11 @@ const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
 const config = require('config');
-
 const app = express();
 
-//bodyParser middleware
 app.use(express.json())
 
 const db = config.get('mongoURI');
-
 mongoose
     .connect(db, {
         useNewUrlParser: true,
@@ -32,6 +29,5 @@ if (process.env.NODE_ENV === 'production') {
     });
 
 }
-
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server started on port ${port}`));
